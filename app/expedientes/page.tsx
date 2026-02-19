4b858f1b5395f44a034bd8ac2982ce79f2976191
adriancabouli <adrian_cabouli@yahoo.com.ar>
2026-02-19 10:47:17 -0300

    cambio

--- app/expedientes/page.tsx
+++ app/expedientes/page.tsx
@@ -605,10 +605,24 @@ export default function ExpedientesPage() {
                       return (
                         <tr key={r.id} className="hover:bg-zinc-50">
                           <td className="px-4 py-3 w-[120px] break-words">
-                            <Link
+                          <Link
                               href={`/expedientes/${r.id}`}
-                              className="font-semibold text-zinc-900 hover:underline"
+                              className="expediente_title_lista inline-flex items-start gap-2 font-semibold text-zinc-900 hover:underline"
                             >
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                strokeWidth="2"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                className="h-4 w-4 shrink-0 mt-[2px] text-[#005e9a]"
+                                aria-hidden="true"
+                              >
+                                <path d="M20 20a2 2 0 0 0 2-2V8a2 2 0 0 0-2-2h-7.9a2 2 0 0 1-1.69-.9L9.6 3.9A2 2 0 0 0 7.93 3H4a2 2 0 0 0-2 2v13a2 2 0 0 0 2 2Z"></path>
+                              </svg>
+
                               {r.expte_code}
                             </Link>
                           </td>
